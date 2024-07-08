c85f2384363de39d92c326c6fbc0c4c6cd023fe8
marcope043@gmail.com <marcope043@gmail.com>
2024-07-08 16:29:36 -0300

    adjusting error

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,7 +1,14 @@
+const appError = require("../utils/appError")
+
 class UsersControllers {
   create(request, response) {
     
     const { name, email, password } = request.body;
+
+    if(!name) {
+      throw new appError("name is required")
+    }
+
     response.status(201).json({ name, email, password });
   }
 }
