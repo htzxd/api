e5d9401b5c819177a7bc26c43178e1057c42da03
marcope043@gmail.com <marcope043@gmail.com>
2024-07-15 13:27:38 -0300

    none

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors")
+
+const appError = require("./utils/appError")
 const express = require("express");
 const  routes  = require("./routes")
 
@@ -6,6 +9,22 @@ app.use(express.json())
 
 app.use(routes);
 
+app.use((error, request, response, next ) => {
+  if(error instanceof appError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+
+    console.error(error)
+
+    return response.status(500).json({
+      status:"error",
+      message: "internal server error"
+    })
+
+  }
+})
 
 const port = 3333;
 app.listen(port, () => console.log(`server is running on port ${port}`))
